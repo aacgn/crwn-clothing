a055f702db41aaa8f50328b2d3abd4f2ac12acd8
aacgn <4augusto@gmail.com>
2020-02-02 13:40:03 -0300

    Update app to redirect to home if user is sign in

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,8 @@
+import { UserActionTypes } from './user.types';
+
 export function setCurrentUser(user) {
     return {
-        type: 'SET_CURRENT_USER',
+        type: UserActionTypes.SET_CURRENT_USER,
         payload: user
     };
 }
